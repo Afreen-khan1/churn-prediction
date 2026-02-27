c91c4cc93b84fc1add3e5f95d55d9948c1be1cb5
Afreen Ahamed <158731991+Afreen-khan1@users.noreply.github.com>
2026-02-27 17:00:08 +0530

    Refactor Flask app to run on specified port
    Updated the app to run on a specified port and removed debug mode.

--- app.py
+++ app.py
@@ -53,7 +53,10 @@ def download():
     return send_from_directory("static", "cleaned_churn_data.xlsx", as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True,use_reloader=False)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host ='0.0.0.0',port=port,debug=False)
+
 
 
 
